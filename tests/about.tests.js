e672eb46b9f2d2bcdf4295a69cb51f6b6c623110
JekoGyulev <165460153+JekoGyulev@users.noreply.github.com>
2026-02-08 20:27:31 +0200

    fix test

--- tests/about.tests.js
+++ tests/about.tests.js
@@ -7,6 +7,6 @@ suite('About page', function() {
     let body = await res.text();
     assert.ok(body.includes("<title>About</title>"));
     assert.ok(body.includes("<h1>About</h1>"));
-    assert.ok(body.includes('<p>This is the "Students Registry" demo app.</p>'));
+    assert.ok(body.includes('<p>This is the "DevOps Registry" demo app.</p>'));
   });
 });
